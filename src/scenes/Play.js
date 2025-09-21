307d8c570fed25ad4cdd90618a935c9334397967
Domenico Pagano <dpaganoh@gmail.com>
2025-09-21 13:30:53 -0500

    mixins

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,7 @@ class Play extends Phaser.Scene {
         const layers = this.createLayer(map);
         const player = this.createPlayer();
 
-        this.physics.add.collider(player, layers.platformsColliders);
+        player.addCollider(layers.platformsColliders);
     }
 
     //Custom methods
