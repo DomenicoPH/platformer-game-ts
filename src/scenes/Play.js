01350760d38b316fc4916b5df25f5091b9358309
Domenico Pagano <dpaganoh@gmail.com>
2025-09-27 22:28:33 -0500

    check the hits

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -38,7 +38,7 @@ class Play extends Phaser.Scene {
         this.graphics.lineStyle(1, 0x00ff00);
 
         this.input.on('pointerdown', this.startDrawing, this);
-        this.input.on('pointerup', this.finishDrawing, this);
+        this.input.on('pointerup', (pointer) => this.finishDrawing(pointer, layers.platforms), this);
     };
 
     startDrawing(pointer){
@@ -47,13 +47,22 @@ class Play extends Phaser.Scene {
         this.line.y1 = pointer.worldY;
     };
 
-    finishDrawing(pointer){
+    finishDrawing(pointer, layer){
         this.line.x2 = pointer.worldX;
         this.line.y2 = pointer.worldY;
-        this.plotting = false;
+        
         this.graphics.clear();
-
         this.graphics.strokeLineShape(this.line);
+        
+        this.tileHits = layer.getTilesWithinShape(this.line);
+        
+        if(this.tileHits.length > 0){
+            this.tileHits.forEach(tile => {
+                tile.index !== -1 && console.log('I have hit the platform')
+            })
+        }
+
+        this.plotting = false;
     };
 
     //Custom methods
